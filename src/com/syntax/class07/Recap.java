34ce1c9b88774d7915c8ea620f2054d40c3c450f
KurbakoAnastasiia <kurbako.anastasia@gmail.com>
2020-10-17 11:27:40 -0400

    second commit

--- src/com/syntax/class07/Recap.java
+++ src/com/syntax/class07/Recap.java
@@ -4,16 +4,50 @@ public class Recap {
 
 	public static void main(String[] args) {
 	
-		String timeOfDay = "Morning";
-		String day = "Monday";
+		String timeOfDay = "Noon";
+		String day = "Saturday";
 		
 		switch (timeOfDay)  {
 		
+		case "Morning":
+			System.out.print("Good Morning");
+			
+			switch (day) {
+			case "Monday":
+				System.out.println(" Colleagues!");
+				break;
+			case "Saturday":
+				System.out.println(" Class!");
+				break;
+			case "Thursday":
+				System.out.println(" Asghar!");
+				break;
+			}
+			break;
 		
+		case "Noon":
+			System.out.print("Good Afternoon");
+			
+			if (day.equals("Monday")) {
+				System.out.println(" Colleagues!");
+			} else if (day.equals("Saturday")) {
+				System.out.println(" Class!");
+			} else if (day.equals("Thursday")) {
+				System.out.println(" Asghar!");
+			}
+
+		break;
 		
+		case "Evening":
+			System.out.println("Good Evening");
+		break;
 		
+		case "Night":
+			System.out.println("Good Night");
+		break;
 		
-		
+		default:
+			System.out.println("N/A");
 		
 		}
 		
