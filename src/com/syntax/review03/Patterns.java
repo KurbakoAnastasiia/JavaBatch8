e2a8bdc236c37f1ced12dab7f07e17790897d5c1
KurbakoAnastasiia <kurbako.anastasia@gmail.com>
2020-10-25 13:03:02 -0400

    Class 11

--- src/com/syntax/review03/Patterns.java
+++ src/com/syntax/review03/Patterns.java
@@ -17,6 +17,28 @@ public class Patterns {
 					}
 						System.out.println();
 	}
-
-}
+					
+					for (int i = 1; i <= 7; i++) {
+						for (int j = 1; j <= i; j++) {
+							System.out.print(j + " ");
+						}
+							System.out.println();
+					}
+					
+					System.out.println("-----------------------------------");
+					 for (int i = 1; i <= 15; i++) {
+					      for (int j = 1; j <= 8-i; j++) {
+					    	  
+					    	  System.out.print(j + " ");
+					      }
+					  	
+					      for (int k = 1; k <= i - 7; k++) {
+					    	 
+					    	  System.out.print(k + " ");
+					      }
+					  	System.out.println();
+					    }
+					 
+					
+					    }
 }
